e08e8a7d3c9a7a8857ef03e34fbeef507e2eeac0
Elmenabawy <ahhossam68@gmail.com>
2024-11-17 17:17:38 +0200

    fix

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -42,30 +42,38 @@ export default function Navbar() {
                 <Link className="nav-link" to="/cart">Cart</Link>
               </li>
 
-
             </ul>
           </> : ''}
           
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            {userToken !== null ? <li className="nav-item">
-              <li className="nav-item d-flex align-items-center">
-                <Link className="nav-link" to="/profile">
-                  <i className="fa-solid fa-user"></i>
-                </Link>
-              </li>
-              <span onClick={()=> logout()} className="nav-link cursor-pointer" >Logout</span>
-            </li> 
-            :<>
+            {userToken !== null ? (
+              <>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/login">Login</Link>
+                  <Link className="nav-link" to="/profile">
+                    <i className="fa-solid fa-user"></i>
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/register">Register</Link>
+                  <span onClick={() => logout()} className="nav-link cursor-pointer">
+                    Logout
+                  </span>
                 </li>
-            </>
-             }
+              </>
+            ) : (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/login">
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/register">
+                    Register
+                  </Link>
+                </li>
+              </>
+            )}
           </ul>
-
         </div>
       </div>
     </nav>
